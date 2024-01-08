f153d93c6eb9f75bb84631dafda730faf03f70b8
MERLIN-CHRISTOPHER <105533249+MERLIN-CHRISTOPHER@users.noreply.github.com>
2024-01-08 13:20:14 +0530

    Add files via upload

---
+++ server1/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "Cr7lSiXvEk5l29g5eOYYls7/WnSrnZm+LQl7mgFuKMA=",
+    EMAIL: "emie.mohr@ethereal.email",
+    PASSWORD:"mSFErxA6nEF1raMEkN",
+    ATLAS_URI:"mongodb+srv://admin:admin123@cluster0.shpkjta.mongodb.net/?retryWrites=true&w=majority"
+}
